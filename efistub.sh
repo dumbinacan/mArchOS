749d1d4bd6be6f00ea2dd2186a5b34ad1783be21
DumbInACan <marco@santana.nyc>
2022-07-03 11:39:55 -0400

    Archimedes boot off zen

--- efistub.sh
+++ efistub.sh
@@ -1,1 +1,1 @@
-efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "boot" --loader /vmlinuz-linux-zen --unicode 'root=UUID=54548113-7fbb-4cc0-a80f-90897499f1b7 rw initrd=\initramfs-linux-zen.img' --verbose
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Archimedes" --loader /vmlinuz-linux-zen --unicode 'root=UUID=be3af5c9-dbb3-409c-b7db-497fba3537ef rw initrd=\initramfs-linux-zen.img' --verbose
